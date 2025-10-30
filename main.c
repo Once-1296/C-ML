1fb47b6c779ae03c5d0ee771d0a8e3c3c8dc0b35
commonman3517 <vvtamhanekar_b24@it.vjti.ac.in>
2025-10-30 13:39:26 +0530

    fixing

--- main.c
+++ main.c
@@ -7,36 +7,35 @@
 int main()
 {
     set_log_level(LOG_LEVEL_INFO);
-    
+    int A=4,B=2,C=1;
     NeuralNetwork *network = create_neural_network(2);
     build_network(network, OPTIMIZER_ADAM, 0.1f, LOSS_MSE, 0.05f, 0.01f);
-    model_add(network, LAYER_DENSE, ACTIVATION_RELU, 5, 52, 0.0f, 0, 0);
-    model_add(network, LAYER_DENSE, ACTIVATION_TANH, 52, 52, 0.0f, 0, 0);
-    model_add(network, LAYER_DENSE, ACTIVATION_SIGMOID, 52, 52, 0.0f, 0, 0);
-model_add(network, LAYER_DENSE, ACTIVATION_TANH, 52, 52, 0.0f, 0, 0);
-model_add(network, LAYER_DENSE, ACTIVATION_TANH, 52, 1, 0.0f, 0, 0);
-    float X_data[52][5];
-    for(int i =0;i<52;i++)
+    model_add(network, LAYER_DENSE, ACTIVATION_TANH, B, A, 0.0f, 0, 0);
+    model_add(network, LAYER_DENSE, ACTIVATION_TANH, A, A, 0.0f, 0, 0);
+model_add(network, LAYER_DENSE, ACTIVATION_TANH, A, C, 0.0f, 0, 0);
+
+    float X_data[A][B];
+    for(int i =0;i<A;i++)
     {
-        for(int j = 0;j<5;j++)
+        for(int j = 0;j<B;j++)
         {
             X_data[i][j]=(1.0f*(1+rand()%(10000))*(1+rand()%(10000))) / 100000000.0f;
         }
     }
 
-    float y_data[52][1];
-    for(int i =0;i<5;i++)
+    float y_data[A][C];
+    for(int i =0;i<B;i++)
     {
         float val =0;
-        for(int j =0;j<52;j++)
+        for(int j =0;j<A;j++)
         {
             val +=(1.f*(j+1))*X_data[i][j];
         }
-        y_data[i][0] = val/(10000.f);
+        y_data[i][0] = val/(100.f);
     }
 
     Dataset *dataset = dataset_create();
-    dataset_load_arrays(dataset, (float *)X_data, (float *)y_data, 52, 100, 1);
+    dataset_load_arrays(dataset, (float *)X_data, (float *)y_data, A, B, 1);
 
     summary(network);
 
